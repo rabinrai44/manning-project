174bf788a3d7d13ee169c6ba6ec83f73fd29b577
Rabin Rai <rabinrai44@gmail.com>
2021-06-20 00:21:11 -0400

    added validation package and annotation for payment request fields

--- salon-api/src/main/java/com/salon/salonapi/payment/models/PaymentRequest.java
+++ salon-api/src/main/java/com/salon/salonapi/payment/models/PaymentRequest.java
@@ -1,19 +1,32 @@
 package com.salon.salonapi.payment.models;
 
+import com.sun.istack.NotNull;
+
 import lombok.Data;
 
-//import javax.validation.constraints.Email;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 
 @Data
 public class PaymentRequest {
 
 	private Long slotId;
 	private Long selectedServiceId;
-	
+
+	@NotNull
+	@Size(min = 1)
 	private String firstName;
+	
+	@NotNull
+	@Size(min = 1)
 	private String lastName;
 	
 
+	@NotNull
+	@Email(message = "Email should be valid")
 	private String email;
+	
+	@NotNull
+	@Size(min = 10)
 	private String phoneNumber;
 }
